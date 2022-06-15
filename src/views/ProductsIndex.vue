1a3b79a95d97829ef8a601de7dd638494b82e8ef
Brian Hasal <brian.hasal@gmail.com>
2022-06-15 05:05:38 -0700

    Added Image to ProductsIndex

--- src/views/ProductsIndex.vue
+++ src/views/ProductsIndex.vue
@@ -25,6 +25,7 @@
   <div class="products-index">
     <h1>Products Index</h1>
     <div v-for="product in products" v-bind:key="product.id">
+      <img v-bind:src="product.primary_image" alt="">
       <h2>{{ product.name }}</h2>
     </div>
   </div>
