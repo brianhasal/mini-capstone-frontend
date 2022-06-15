3741a3bb5e4986c9442beeab48d9735ea031ce50
Brian Hasal <brian.hasal@gmail.com>
2022-06-14 19:13:37 -0700

    Configure Netlify redirects

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SignupView from "../views/SignupView.vue";
+import LoginView from "../views/LoginView.vue";
+import LogoutView from "../views/LogoutView.vue";
+import ProductsIndex from "../views/ProductsIndex.vue";
 
 const routes = [
   {
@@ -14,7 +18,30 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: SignupView
+  },
+  { 
+    path: "/login", 
+    name: "login", 
+    component: LoginView 
+  },
+  { 
+    path: "/logout", 
+    name: "logout", 
+    component: LogoutView 
+  },
+  { 
+    path: "/products", 
+    name: "products-index", 
+    component: ProductsIndex 
+  },
 ]
 
 const router = createRouter({
---
+++ src/views/ProductsIndex.vue
@@ -0,0 +1,33 @@
+<script>
+  import axios from "axios";
+
+  export default {
+    data: function () {
+      return {
+        products: [],
+      };
+    },
+    created: function () {
+      this.indexProducts();
+    },
+    methods: {
+      indexProducts: function () {
+        axios.get("/products.json").then((response) => {
+          console.log("products index", response);
+          this.products = response.data;
+        });
+      },
+    },
+  };
+</script>
+
+<template>
+  <div class="products-index">
+    <h1>Products Index</h1>
+    <div v-for="product in products" v-bind:key="product.id">
+      <h2>{{ product.name }}</h2>
+    </div>
+  </div>
+</template>
+
+<style></style>
